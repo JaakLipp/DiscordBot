a944069538702ad9cf74ec6daa476af3c6213bad
JaakLipp <93842534+JaakLipp@users.noreply.github.com>
2022-05-13 17:59:35 -0400

    Add files via upload

---
+++ phasmo_cog.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+import random
+
+'''
+For fun modifications of the game "Phasmophobia"
+'''
+class phasmo_cog(commands.Cog):
+
+    def __init__(self, bot):
+        self.list_of_items = ['Smudge Stick', 'Candle', 'EMF', 'UV', 'Motion Sensor', 'Tripod',
+                              'Video Camera', 'Photo Camera', 'Sound sensor', 'Parabolic Microphone',
+                              'Ghost Writing Book', 'Spirit Box', 'Glow Stick', 'DOTS', 'Salt',
+                              'Sanity Pills', 'Thermometer', 'Crucifix']
+
+    '''
+    Gets a random item from a list of items for a person to use in-game
+    '''
+    @commands.command(name='item')
+    async def item(self, ctx):
+        await ctx.send('You get' + random.choice(self.list_of_items))
+
